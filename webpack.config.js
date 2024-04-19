aab8b816f6682c8419f06394e0878f0292334f29
ViniciusLCLima <vinicius.docarmo1998@gmail.com>
2024-04-19 13:40:06 -0300

    Make independent build folder be created

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require("path")
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     mode: "development",
     devtool: false,
     output:{
-        filename: "main.js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader, css-loader']
+            }
+        ]
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })]
 }
